bb1642f230723dc5dc3f9d93f5cedf3cc6f35b5a
Terrambc <86408053+Terrambc@users.noreply.github.com>
2025-06-13 16:15:06 +0200

    Download des Datensatz von Huggingface zum Trainieren

---
+++ BHT_BA2025_LLM_Karpathy/fineweb.py
@@ -0,0 +1,92 @@
+"""
+FineWeb-Edu dataset (for srs pretraining)
+https://huggingface.co/datasets/HuggingFaceFW/fineweb-edu
+Downloads and tokenizes the data and saves data shards to disk.
+Run simply as:
+$ python fineweb.py
+Will save shards to the local directory "edu_fineweb10B".
+"""
+
+import os
+import multiprocessing as mp
+import numpy as np
+import tiktoken
+from datasets import load_dataset
+from tqdm import tqdm 
+
+
+# ------------------------------------------
+local_dir = "edu_fineweb10B"
+remote_name = "sample-10BT"
+shard_size = int(1e8) # 100 Millionen Token pro Shard, insgesamt 100 Shards
+
+# Erstellen Sie den Cache im lokalen Verzeichnis, falls dieser noch nicht existiert
+DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
+os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+
+# download den Datensatz
+fw = load_dataset("HuggingFaceFW/fineweb-edu", name=remote_name, split="train", streaming=True)
+
+# Inizialisiert den Tokenizer
+enc = tiktoken.get_encoding("gpt2")
+eot = enc._special_tokens['<|endoftext|>'] # end of text token
+def tokenize(doc):
+    # tokenisiert ein einzelnes Dokument und gibt ein Numpy-Array von uint16-Token zurück
+    tokens = [eot] # the special <|endoftext|> token delimits all documents
+    tokens.extend(enc.encode_ordinary(doc["text"]))
+    tokens_np = np.array(tokens)
+    assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
+    tokens_np_uint16 = tokens_np.astype(np.uint16)
+    return tokens_np_uint16
+
+def write_datafile(filename, tokens_np):
+    np.save(filename, tokens_np)
+
+
+
+
+def main():
+    # Tokenisiert alle Dokumente und schreibt Ausgabe-Shards, jeweils mit Token der Größe shard_size (der letzte Shard hat den Rest)
+    nprocs = max(1, os.cpu_count()//2)
+    with mp.Pool(nprocs) as pool:
+        shard_index = 0
+        # Puffer vorab zuordnen, um den aktuellen Shard zu halten
+        all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
+        token_count = 0
+        progress_bar = None
+        for tokens in pool.imap(tokenize, fw, chunksize=16):
+
+            # Ist im aktuellen Shard genügend Platz für die neuen Token?
+            if token_count + len(tokens) < shard_size:
+                # fügt einfach Token zum aktuellen Shard hinzu
+                all_tokens_np[token_count:token_count+len(tokens)] = tokens
+                token_count += len(tokens)
+                # update progress bar
+                if progress_bar is None:
+                    progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_index}")
+                progress_bar.update(len(tokens))
+            else:
+                # Schreibe den aktuellen Shard und starte einen neuen
+                split = "val" if shard_index == 0 else "train"
+                filename = os.path.join(DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}")
+                # das Dokument in die Teile aufteilen, die in diesen Shart passen; der Rest geht in den nächsten
+                remainder = shard_size - token_count
+                progress_bar.update(remainder)
+                all_tokens_np[token_count:token_count+remainder] = tokens[:remainder]
+                write_datafile(filename, all_tokens_np)
+                shard_index += 1
+                progress_bar = None
+                # Füllt den nächsten Shard mit den Resten des aktuellen Dokuments
+                all_tokens_np[0:len(tokens)-remainder] = tokens[remainder:]
+                token_count = len(tokens)-remainder
+
+        # schreibe alle verbleibenden Token als letztes Shard
+        if token_count != 0:
+            split = "val" if shard_index == 0 else "train"
+            filename = os.path.join(DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}")
+            write_datafile(filename, all_tokens_np[:token_count])
+
+
+
+if __name__ == "__main__":
+    main()
